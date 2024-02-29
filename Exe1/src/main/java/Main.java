47adaf67ecb07de71a19e013334f5c0f08fa0aaa
Gabriel <Gabriel@DESKTOP-BOA4N64>
2024-02-29 16:35:40 -0300

    1st commit

---
+++ Exe1/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("a: ");
+		a = scanner.nextInt();
+		
+		System.out.println("b: ");
+		b = scanner.nextInt();
+		
+		System.out.println("Soma:" + (a+b));
+		
+	}
+
+}
